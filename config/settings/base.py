405acca11b4bf1501090686acba2955ae7ae5b1c
Volodymyr Tartynskyi <fon.vosi@gmail.com>
2020-12-22 05:48:10 +0200

    add sentry

--- config/settings/base.py
+++ config/settings/base.py
@@ -5,7 +5,7 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = 'g#$0(*8%8af27k7-e!ll^!-4yxomcx8ljv_o&_z*zhvi)f8&e7'
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 SITE_ID = 1
