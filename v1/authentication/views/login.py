df7b2c65a3010d8aa8060e555839852b30e25e68
Bucky Roberts <buckyroberts@gmail.com>
2020-12-29 17:21:05 -0500

    Restructure response for ease of normalizr

--- v1/authentication/views/login.py
+++ v1/authentication/views/login.py
@@ -23,9 +23,11 @@ class LoginView(APIView):
 
         return Response(
             {
-                **UserSerializer(user).data,
-                'access_token': str(refresh_token.access_token),
-                'refresh_token': str(refresh_token),
+                'user': UserSerializer(user).data,
+                'authentication': {
+                    'access_token': str(refresh_token.access_token),
+                    'refresh_token': str(refresh_token),
+                }
             },
 
         )
