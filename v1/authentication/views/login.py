83ab94f288e6b773c6c081c862848a264f47330f
Bucky Roberts <buckyroberts@gmail.com>
2020-12-30 23:36:46 -0500

    Updates for Redux

--- v1/authentication/views/login.py
+++ v1/authentication/views/login.py
@@ -23,9 +23,11 @@ class LoginView(APIView):
 
         return Response(
             {
-                **UserSerializer(user).data,
-                'access_token': str(refresh_token.access_token),
-                'refresh_token': str(refresh_token),
+                'authentication': {
+                    'access_token': str(refresh_token.access_token),
+                    'refresh_token': str(refresh_token)
+                },
+                'user': UserSerializer(user).data,
             },
 
         )
