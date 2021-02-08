2772d0bf768b308485d344bea71f8e98ae3d2b11
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-08 17:07:41 +0545

    going with the code standard again

--- v1/teams/models/slack_channel.py
+++ v1/teams/models/slack_channel.py
@@ -5,10 +5,11 @@ from thenewboston.models.created_modified import CreatedModified
 
 from v1.teams.models.team import Team
 
+
 class SlackChannel(CreatedModified):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    name  = models.CharField(max_length=250)
-    team  = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
+    name = models.CharField(max_length=250)
+    team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f'#{self.pk}: {self.title}'
