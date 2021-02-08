7751a9142cdedaf5afeb944cc90b4b08bc364ca4
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-08 15:18:25 +0545

    Team Page API Updates #27

--- v1/teams/models/slack_channel.py
+++ v1/teams/models/slack_channel.py
@@ -1,18 +1,17 @@
 import uuid
 
 from django.db import models
-from v1.repositories.models.repository import Repository
+from v1.teams.models.team import Team
 from thenewboston.models.created_modified import CreatedModified
 
 
-class Team(CreatedModified):
+class SlackChannel(CreatedModified):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    title = models.CharField(max_length=250)
-    about  = models.TextField()
-    responsibilities  = models.TextField()
+    name  = models.CharField(max_length=250)
+    team  = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f'#{self.pk}: {self.title}'
 
     class Meta:
-        ordering = ('created_date', 'title')
+        ordering = ('created_date', 'name')
