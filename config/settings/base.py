b5103cac824e0f2fc2d2fa936186e966ace4325e
Justin Ndwiga <ndwigajustin@gmail.com>
2021-02-09 19:44:12 +0300

    add secret key to template.yml

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
