742b19671d52b75ab89c2a7f9f8627c91810a3eb
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-09 21:42:06 +0545

    replaced DO_NOTHING with CASCADE

--- v1/teams/models/slack_channel.py
+++ v1/teams/models/slack_channel.py
@@ -9,7 +9,7 @@ from v1.teams.models.team import Team
 class SlackChannel(CreatedModified):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     name = models.CharField(max_length=250)
-    team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'#{self.pk}: {self.title}'
