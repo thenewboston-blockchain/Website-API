3559b356d942dd2913185efb16723706da9084a8
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-02-09 21:42:06 +0545

    replaced DO_NOTHING with CASCADE

--- v1/repositories/models/repository.py
+++ v1/repositories/models/repository.py
@@ -1,6 +1,6 @@
 import uuid
 
-from django.db.models import CharField, DO_NOTHING, ForeignKey, URLField, UUIDField
+from django.db.models import CharField, CASCADE, ForeignKey, URLField, UUIDField
 from thenewboston.models.created_modified import CreatedModified
 
 from v1.teams.models.team import Team
@@ -10,7 +10,7 @@ class Repository(CreatedModified):
     uuid = UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     url = URLField(blank=True)
     display_name = CharField(max_length=80, unique=True)  # TNB-python-client
-    team = ForeignKey(Team, on_delete=DO_NOTHING)
+    team = ForeignKey(Team, on_delete=CASCADE)
 
     class Meta:
         verbose_name_plural = 'repositories'
