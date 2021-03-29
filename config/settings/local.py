59805d2bef96817f7539936f3af7808e790454ee
Justin Ndwiga <ndwigajustin@gmail.com>
2021-03-29 20:03:51 +0300

    remove logging config

--- config/settings/local.py
+++ config/settings/local.py
@@ -10,24 +10,6 @@ INTERNAL_IPS = iptools.IpRangeList(
     '172.16/12',
     '192.168/16'
 )
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/nginx/error.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
 INSTALLED_APPS.append('debug_toolbar')
 
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
