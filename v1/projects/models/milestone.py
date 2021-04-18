ac13453175f5df40994730fdcbfe18b4bf16cf67
Justin Ndwiga <ndwigajustin@gmail.com>
2021-04-18 11:51:02 +0300

    add project milestones

---
+++ v1/projects/models/milestone.py
@@ -0,0 +1,17 @@
+import uuid
+
+from django.db import models
+from thenewboston.models.created_modified import CreatedModified
+
+
+class Milestone(CreatedModified):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    project = models.ForeignKey('projects.Project', on_delete=models.CASCADE)
+    number = models.PositiveIntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'#{self.pk}: {self.number}'
+
+    class Meta:
+        ordering = ('created_date', 'number')
