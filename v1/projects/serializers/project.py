f74cdedde0fbfbfab8dae0c9432d816a3d4fcf4a
Justin Ndwiga <ndwigajustin@gmail.com>
2021-04-24 20:34:15 +0300

    list project milestones

--- v1/projects/serializers/project.py
+++ v1/projects/serializers/project.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
 from ..models.project import Project
+from ..serializers.milestone import MilestoneSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    milestones = serializers.SerializerMethodField()
+
     class Meta:
         fields = '__all__'
         model = Project
         read_only_fields = 'created_date', 'modified_date'
+
+    def get_milestones(self, project):
+        return MilestoneSerializer(project.milestone_set.all(), many=True).data
