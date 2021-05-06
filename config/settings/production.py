2642c3c050f4c489b5257d958e992d978bd2e772
Truyen <Truyenhv@outlook.com>
2021-05-06 12:10:59 +0700

    Fix production config
    update
    
    
    update
    
    
    update

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,5 @@
 import iptools
+
 from .base import *  # noqa: F401
 
 SENTRY_DSN = os.getenv('SENTRY_DSN')
@@ -16,8 +17,8 @@ if SENTRY_DSN:
 DEBUG = False
 
 INTERNAL_IPS = iptools.IpRangeList(
-   '10/8',
-   '127/8',
-   '172.16/12',
-   '192.168/16'
+    '10/8',
+    '127/8',
+    '172.16/12',
+    '192.168/16'
 )
