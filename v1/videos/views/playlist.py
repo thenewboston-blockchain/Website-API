5a3576e263db7f341f8aad4ca0e8ecb2d27bd7f6
Bucky <buckyroberts@gmail.com>
2021-05-27 20:26:46 -0700

    Cleaning

--- v1/videos/views/playlist.py
+++ v1/videos/views/playlist.py
@@ -12,9 +12,9 @@ from ...third_party.rest_framework.permissions import IsStaffOrReadOnly
 
 class PlaylistViewSet(viewsets.ModelViewSet):
     queryset = Playlist.objects \
-        .select_related('instructor')\
+        .select_related('instructor') \
         .prefetch_related(Prefetch('videos')) \
-        .order_by('created_date') \
+        .order_by('title') \
         .all()
     permission_classes = [IsStaffOrReadOnly]
 
@@ -29,25 +29,48 @@ class PlaylistViewSet(viewsets.ModelViewSet):
         if request.query_params.get('category') or request.query_params.get('instructor'):
             category = request.query_params.get('category')
             instructor = request.query_params.get('instructor')
+
             if category and not instructor:
                 try:
                     category = PlaylistCategory.objects.get(name__iexact=category)
-                    playlists = Playlist.objects.filter(categories__pk=category.pk).select_related('instructor').prefetch_related(Prefetch('videos')).order_by('created_date')
+                    playlists = Playlist.objects.filter(
+                        categories__pk=category.pk
+                    ).select_related(
+                        'instructor'
+                    ).prefetch_related(
+                        Prefetch('videos')
+                    ).order_by('created_date')
                 except PlaylistCategory.DoesNotExist:
-                    return Response({'detail': 'No playlist under category: {} was found'.format(category)}, status=status.HTTP_404_NOT_FOUND)
+                    return Response(
+                        {'detail': f'No playlist under category: {category} was found'},
+                        status=status.HTTP_404_NOT_FOUND
+                    )
             elif instructor and not category:
                 try:
                     playlists = Playlist.objects.filter(instructor__pk=instructor).order_by('created_date')
                 except Instructor.DoesNotExist:
-                    return Response({'detail': 'No playlist under instructor: {} was found'.format(instructor)}, status=status.HTTP_404_NOT_FOUND)
+                    return Response(
+                        {'detail': f'No playlist under instructor: {instructor} was found'},
+                        status=status.HTTP_404_NOT_FOUND
+                    )
             elif category and instructor:
                 try:
                     category = PlaylistCategory.objects.get(name__iexact=category)
-                    playlists = Playlist.objects.filter(categories__pk=category.pk, instructor__pk=instructor).order_by('created_date')
+                    playlists = Playlist.objects.filter(
+                        categories__pk=category.pk,
+                        instructor__pk=instructor
+                    ).order_by('created_date')
                 except PlaylistCategory.DoesNotExist:
-                    return Response({'detail': 'No playlist under category: {} was found'.format(category)}, status=status.HTTP_404_NOT_FOUND)
+                    return Response(
+                        {'detail': f'No playlist under category: {category} was found'},
+                        status=status.HTTP_404_NOT_FOUND
+                    )
                 except ValidationError:
-                    return Response({'detail': '{} is not a valid instructor uuid'.format(instructor)}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {'detail': '{instructor} is not a valid instructor uuid'},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
+
             playlists = self.filter_queryset(playlists)
             page = self.paginate_queryset(playlists)
             serializer = self.get_serializer_class()
@@ -55,7 +78,7 @@ class PlaylistViewSet(viewsets.ModelViewSet):
 
         else:
             queryset = Playlist.objects \
-                .select_related('instructor')\
+                .select_related('instructor') \
                 .prefetch_related(Prefetch('videos')) \
                 .order_by('created_date') \
                 .all()
