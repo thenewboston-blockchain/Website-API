dddaaea8eaf78fbd38a632565e588241a21ed2fa
Justin Ndwiga <ndwigajustin@gmail.com>
2021-06-08 11:20:37 +0300

    make email write_only

--- v1/feedback/serializers/feedback.py
+++ v1/feedback/serializers/feedback.py
@@ -8,4 +8,7 @@ class FeedbackSerializer(ModelSerializer):
     class Meta:
         fields = ('pk', 'name', 'email', 'message', 'created_date', 'modified_date',)
         read_only_fields = ('created_date', 'modified_date',)
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
         model = Feedback
