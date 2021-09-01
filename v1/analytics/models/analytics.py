b395e426ceed672c67c4bedd05baa7d2b0b5d860
Justin Ndwiga <ndwigajustin@gmail.com>
2021-09-01 14:46:48 +0300

    show category title in analytics

--- v1/analytics/models/analytics.py
+++ v1/analytics/models/analytics.py
@@ -34,7 +34,9 @@ class Analytics(CreatedModified):
         verbose_name_plural = 'analytics'
 
     def __str__(self):
-        return f'#{self.pk}: {self.title}'
+        category = AnalyticsCategory.objects.get(analytics__in=[self.pk])
+        category_title = ': ' + category.title if category else ''
+        return f'#{self.pk} {category_title} : {self.title}'
 
     def save(self, *args, **kwargs):
         cache.delete_pattern('views.decorators.cache.cache*')
